1b6e7f102832777195bafa8cb72fca002bd78cac
Andrés Villalobos <andresalvivar@gmail.com>
2022-09-22 01:50:28 -0600

    Hm, first time trying this. I'll try to make it work.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
 use axum::response::Response;
 use axum::routing::get;
 use axum::Router;
+use futures::io::WriteHalf;
 use futures::stream::{SplitSink, SplitStream, StreamExt};
 use tokio::net::TcpStream;
-use tokio::sync::mpsc;
+use tokio::sync::mpsc::{self, UnboundedSender};
 
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
@@ -35,28 +36,31 @@ async fn handler(ws: WebSocketUpgrade, state: Arc<Mutex<State>>) -> Response {
     thread::spawn(move || loop {});
 
     println!("New connection: {:?}", ws);
-    ws.on_upgrade(|ws| handle_socket(ws, state))
+    ws.on_upgrade(|ws| init_sockets(ws, state))
 }
 
-async fn handle_socket(socket: WebSocket, state: Arc<Mutex<State>>) {
+async fn init_sockets(socket: WebSocket, state: Arc<Mutex<State>>) {
     let mut state = state.lock().unwrap();
     state.count += 1;
     drop(state);
 
-    let bite_addr = SocketAddr::from(([127, 0, 0, 1], 1984));
-    tokio::spawn(handle_bite(bite_addr));
-
     let (bite_sender, bite_receiver) = mpsc::unbounded_channel::<Command>();
+    let (ws_sender, ws_receiver) = mpsc::unbounded_channel::<Command>();
 
-    let (ws_sender, ws_receiver) = socket.split();
-    tokio::spawn(ws_writer(ws_sender));
-    tokio::spawn(ws_reader(ws_receiver));
+    let bite_addr = SocketAddr::from(([127, 0, 0, 1], 1984));
+    tokio::spawn(handle_bite(bite_addr, ws_sender));
+    tokio::spawn(handle_websocket(socket));
+
+    // tokio::spawn(ws_writer(ws_sender));
+    // tokio::spawn(ws_reader(ws_receiver));
 }
 
-async fn handle_bite(addr: SocketAddr) {
+async fn handle_bite(addr: SocketAddr, ws_sender: UnboundedSender<Command>) {
     match TcpStream::connect(addr).await {
         Ok(mut tcp) => {
             let (tcp_sender, tcp_receiver) = tcp.split();
+            // tokio::spawn(bite_reader(tcp_receiver));
+            // tokio::spawn(bite_writer(tcp_sender));
         }
 
         Err(e) => {
@@ -64,6 +68,14 @@ async fn handle_bite(addr: SocketAddr) {
         }
     }
 }
+
+async fn handle_websocket(socket: WebSocket) {
+    let (ws_sender, ws_receiver) = socket.split();
+}
+
+async fn bite_reader(bite_receiver: WriteHalf<TcpStream>) {}
+
+async fn bite_writer(mut sender: UnboundedSender<Command>) {}
 
 async fn ws_reader(mut receiver: SplitStream<WebSocket>) {
     loop {
